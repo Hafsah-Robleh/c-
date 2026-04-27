8b054294d6b9b4c81ff4754e16effc0afb0ec0b3
Hafsah <hafsah.robleh@gmail.com>
2026-04-27 16:02:55 -0400

    array

---
+++ array.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+//#include <fstream> 
+//#include <string> 
+
+
+using namespace std;
+
+int main() {
+
+    const int NUM_OF_LETTERS = 5;
+    char letters [NUM_OF_LETTERS];
+    cout << " enter 5 numbers";
+    for letters (index = 0; index < NUM_OF_LETTERS, index++) {
+        cin >> letters[index];
+    }
+
+}
